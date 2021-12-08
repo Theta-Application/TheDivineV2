c7e5318e4033740e0a334338b62322f314f15288
anthonyvigz <anthonyvigz@gmail.com>
2021-12-08 16:23:24 -0500

    fix: site so far

--- the-divine-v2/src/components/Home.jsx
+++ the-divine-v2/src/components/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import AbsoluteWrapper from "./AbsoluteWrapper";
-import resume from "../img/AnthonyVigliottaResume.pdf";
-import "../css/resume.css";
+import os from "../img/os.png";
 
 function Home() {
   document.body.classList.remove("newpage");
@@ -9,38 +8,25 @@ function Home() {
   return (
     <AbsoluteWrapper>
       <div className="homeBlock">
-        <button className="hello">Hello!</button>
-        <h2>I am Anthony.</h2>
+        <a
+          href="https://opensea.io/collection/the-divine-nft"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="osButton">
+            <img src={os} alt="opensea" id="os" />
+            <span id="rightarrow" className="bounce">
+              <i className="fas fa-chevron-right"></i>
+            </span>
+          </div>
+        </a>
+        <h2>Fight for the Heavens.</h2>
         <p>
-          I am a Full Stack Web Developer highly skilled in an array of
-          front-end and back-end languages and frameworks. I have a passion for
-          crisp design and flawless functionality.
+          4,444 hand-drawn, cosmic, angelic warriors that protect and nurture
+          all that is good in the observable universe of their realm.
         </p>
-        <div className="middle">
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://github.com/anthonyvigz"
-          >
-            <i className="fab fa-github"></i>
-          </a>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.linkedin.com/in/anthonyvigliotta/"
-          >
-            <i className="fab fa-linkedin-in"></i>
-          </a>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://twitter.com/anthonyvigz"
-          >
-            <i className="fab fa-twitter"></i>
-          </a>
-          <a target="_blank" rel="noopener noreferrer" href={resume}>
-            <i className="fas fa-file"></i>
-          </a>
+        <div className="mintBlock">
+          <button className="shine-box">MINT</button>
         </div>
       </div>
     </AbsoluteWrapper>
