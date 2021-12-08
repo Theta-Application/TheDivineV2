6f85bd3f67e842c005af29453827c0bf8f94a2c2
anthonyvigz <anthonyvigz@gmail.com>
2021-12-08 16:51:22 -0500

    fix: divine

--- the-divine-v2/src/components/Home.jsx
+++ the-divine-v2/src/components/Home.jsx
@@ -1,9 +1,95 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AbsoluteWrapper from "./AbsoluteWrapper";
 import os from "../img/os.png";
+import {
+  connectWallet,
+  getCurrentWalletConnected,
+  mintNFT,
+  presaleMints,
+  totalSupply,
+} from "../utils/interact";
 
 function Home() {
+  // Minter information
+  const [walletAddress, setWallet] = useState("");
+  const [status, setStatus] = useState("");
+
+  const [supply, setSupply] = useState(0);
+
+  const [, setPresaleTotalMints] = useState(0);
+
+  // Mint amount
+  const [mintAmount, setMintAmount] = useState(1);
+
   document.body.classList.remove("newpage");
+
+  function addWalletListener() {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts.length > 0) {
+          setWallet(accounts[0]);
+        } else {
+          setWallet("");
+          setStatus("🦊 Connect to Metamask using the top right button.");
+        }
+      });
+    } else {
+      setStatus(
+        <p>
+          {" "}
+          🦊{" "}
+          <a
+            target="_blank"
+            href={`https://metamask.io/download.html`}
+            rel="noreferrer"
+          >
+            You must install Metamask, a virtual Ethereum wallet, in your
+            browser.
+          </a>
+        </p>
+      );
+    }
+  }
+
+  useEffect(() => {
+    async function fetchData() {
+      const { address, status } = await getCurrentWalletConnected();
+      const supply = await totalSupply();
+      setSupply(supply);
+      setWallet(address);
+      setStatus(status);
+
+      addWalletListener();
+    }
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const setMintMessage = async () => {
+      setPresaleTotalMints(await presaleMints(walletAddress));
+    };
+    if (walletAddress) {
+      setMintMessage();
+    }
+  }, [walletAddress]);
+
+  const updateMintAmount = () => {
+    if (mintAmount !== 12) {
+      setMintAmount(mintAmount + 1);
+    } else {
+      setMintAmount(1);
+    }
+  };
+
+  const connectWalletPressed = async () => {
+    const walletResponse = await connectWallet();
+    setStatus(walletResponse.status);
+    setWallet(walletResponse.address);
+  };
+
+  const onMintPressed = async () => {
+    mintNFT(mintAmount, walletAddress);
+  };
 
   return (
     <AbsoluteWrapper>
@@ -25,8 +111,22 @@ function Home() {
           4,444 hand-drawn, cosmic, angelic warriors that protect and nurture
           all that is good in the observable universe of their realm.
         </p>
-        <div className="mintBlock">
-          <button className="shine-box">MINT</button>
+        <div className="allMintingInfo">
+          <div className="mintBox">
+            <button onClick={onMintPressed} className="shine-box">MINT</button>
+          </div>
+          <div className="totalAndConnect">
+            <button onClick={connectWalletPressed}>CONNECT</button>
+            <button onClick={updateMintAmount}>
+              {mintAmount} DIVINE{mintAmount === 1 ? "" : "S"}
+            </button>
+          </div>
+        </div>
+        <div className="supply">
+          <h2>
+            <span style={{ color: "gold " }}>{supply}</span>/4444 warriors are
+            ready.
+          </h2>
         </div>
       </div>
     </AbsoluteWrapper>
---
+++ the-divine-v2/src/utils/interact.js
@@ -0,0 +1,179 @@
+import React from 'react';
+
+require("dotenv").config();
+
+const Web3 = require("web3");
+const ethNetwork =
+  "https://mainnet.infura.io/v3/f614a724e563453f9105be481807abb3";
+const web3 = new Web3(new Web3.providers.HttpProvider(ethNetwork));
+
+const contractABI = require("./contract-abi.json");
+const contractAddress = "0xA3C2E31ad437742173706578748A223DdA95F019"
+
+export const connectWallet = async () => {
+  if (window.ethereum) {
+    try {
+      const addressArray = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      const obj = {
+        status: "Wallet connected, change your wallet by entering Metamask 🦊",
+        address: addressArray[0],
+      };
+      return obj;
+    } catch (err) {
+      return {
+        address: "",
+        status: "😥 " + err.message,
+      };
+    }
+  } else {
+    return {
+      address: "",
+      status: (
+        <span>
+          <p>
+            {" "}
+            🦊{" "}
+            <a
+              target="_blank"
+              href={`https://metamask.io/download.html`}
+              rel="noreferrer"
+            >
+              You must install Metamask, a virtual Ethereum wallet, in your
+              browser.
+            </a>
+          </p>
+        </span>
+      ),
+    };
+  }
+};
+
+export const getCurrentWalletConnected = async () => {
+  if (window.ethereum) {
+    try {
+      const addressArray = await window.ethereum.request({
+        method: "eth_accounts",
+      });
+      if (addressArray.length > 0) {
+        return {
+          address: addressArray[0],
+          status: "Wallet connected, change your wallet by entering Metamask 🦊",
+        };
+      } else {
+        return {
+          address: "",
+          status: "🦊 Connect to Metamask using the top right button.",
+        };
+      }
+    } catch (err) {
+      return {
+        address: "",
+        status: "😥 " + err.message,
+      };
+    }
+  } else {
+    return {
+      address: "",
+      status: (
+        <span>
+          <p>
+            {" "}
+            🦊{" "}
+            <a
+              target="_blank"
+              href={`https://metamask.io/download.html`}
+              rel="noreferrer"
+            >
+              You must install Metamask, a virtual Ethereum wallet, in your
+              browser.
+            </a>
+          </p>
+        </span>
+      ),
+    };
+  }
+};
+
+export const totalSupply = async () => {
+
+  const updatedAddress = web3.utils.toChecksumAddress(
+    contractAddress
+  );
+
+  window.contract = await new web3.eth.Contract(contractABI, updatedAddress);
+
+  const totalSupply = await window.contract.methods.totalSupply().call();
+
+  return totalSupply;
+}
+
+export const presaleMints = async (wallet) => {
+
+  const updatedAddress = web3.utils.toChecksumAddress(
+    contractAddress
+  );
+
+  window.contract = await new web3.eth.Contract(contractABI, updatedAddress);
+
+  console.log("WALLET", wallet)
+
+  const whitelistedTotal = await window.contract.methods.Whitelist(wallet).call();
+
+  console.log("whitelistedTotal", whitelistedTotal)
+
+  return whitelistedTotal;
+};
+
+export const mintNFT = async (mintAmount, address) => {
+
+  if (!address) {
+    return;
+  }
+
+  console.log("CONTRACT ABI", contractABI);
+
+  const updatedAddress = web3.utils.toChecksumAddress(
+    contractAddress
+  );
+
+  window.contract = await new web3.eth.Contract(contractABI, updatedAddress);
+
+  console.log("window contract", window.contract);
+
+  console.log("mint amount", mintAmount, web3.utils.toWei("0.08"));
+
+  const value = String(mintAmount * web3.utils.toWei("0.08"));
+
+  console.log("value", value, typeof value);
+
+  //set up your Ethereum transaction
+  const transactionParameters = {
+    to: updatedAddress, // Required except during contract publications.
+    from: window.ethereum.selectedAddress, // must match user's active address.
+    value: parseInt(web3.utils.toWei("0.08", "ether") * mintAmount).toString(
+      16
+    ),
+    data: window.contract.methods
+      .mint(mintAmount)
+      .encodeABI(), //make call to NFT smart contract
+  };
+
+  //sign the transaction via Metamask
+  try {
+    const txHash = await window.ethereum.request({
+      method: "eth_sendTransaction",
+      params: [transactionParameters],
+    });
+    return {
+      success: true,
+      status: "✅ Check out your transaction on Etherscan:" + txHash,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      status: "😥 Something went wrong: " + error.message,
+    };
+  }
+};
