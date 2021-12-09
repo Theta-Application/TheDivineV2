040d4aa66ccff77f70bd6735210747a27691c4b4
anthonyvigz <anthonyvigz@gmail.com>
2021-12-09 14:02:39 -0500

    fix: working tiers

--- the-divine-v2/src/components/Tiers.jsx
+++ the-divine-v2/src/components/Tiers.jsx
@@ -3,6 +3,11 @@ import AbsoluteWrapper from "./AbsoluteWrapper";
 import "../css/tiers.css";
 import { Transition } from "react-transition-group";
 import divine from "../img/BACKGROUNDDIVINE.png";
+import vindicator from "../img/Vindicator.png";
+import apostle from "../img/Apostle.png";
+import royalty from "../img/Royalty.png";
+import valkyrie from "../img/Valkyrie.png";
+import trinity from "../img/Trinity.png";
 
 function Tiers() {
   const [switcher, setSwitch] = useState(false);
@@ -10,21 +15,39 @@ function Tiers() {
   const types = [
     {
       tierName: "THE VINDICATORS",
+      img: vindicator,
+      number: 1,
+      text: "The Vindicators are souls torn from The Arc. During the Eradication, the Arc realized they were outnumbered and forced their soul into separate beings to independently grow stronger. The downside, they are mortal, and many were lost in the battle. The remaining Vindicators stand ready for the next Eradication and ready to give their life protecting the Heavens.",
     },
     {
       tierName: "THE APOSTLES",
+      img: apostle,
+      number: 2,
+      text: "The Apostles were humans turned immortal as soon as their lives were to end. They were chosen by The Arc because of the skills and benevolence they conjured in their mortal lives. Their skills, now enhanced on a cosmic and magical scale, were and are extremely powerful weapons for the fight in the first Eradication, and the next.",
     },
     {
       tierName: "THE ROYALTY",
+      img: royalty,
+      number: 3,
+      text: "The Royalty were Apostles that have been to, and seen, The Fire, but miraculously escaped. Their damaged, but now all evil-knowing minds, have proven useful to the other Apostles' studies. They are retired, worshipped, and revered; they will now only speak on the evils if the information were to aid the Divine in the next Eradication. Is there something they are hiding about The Fire?",
     },
     {
       tierName: "THE VALKYRIE",
+      img: valkyrie,
+      number: 4,
+      text: "Each of them harnessing the power of a unique, elemental force, the Valkyrie are a small group of celestial warriors that mysteriously appeared from the Void. They warn the Divine of the next Eradication and how it's not going to be like the fight they had the first time. Their strengths and power have been demonstrated to almost match those of the Trinity, which stirs an uneasiness amongst the Divine.",
     },
     {
       tierName: "THE TRINITY",
+      img: trinity,
+      number: 5,
+      text: "Okay",
     },
     {
       tierName: "THE ARC",
+      img: "",
+      number: 6,
+      text: "Okay",
     },
   ];
 
@@ -107,7 +130,7 @@ function Tier(props) {
     zIndex: -5,
   };
 
-  const { tierName } = props.divine;
+  const { tierName, img, number, text } = props.divine;
 
   return (
     <Transition in={props.switcher} timeout={duration}>
@@ -119,20 +142,13 @@ function Tier(props) {
           }}
         >
           <div className="tier">
-            <h1 className="tierNumber">Tier 1</h1>
+            <h1 className="tierNumber">Tier {number}</h1>
             <h2 className="tierType">{tierName}</h2>
             <div className="imageAndBorder">
-              <img src={divine} alt="divine" />
+              {!(number === 6) && <img src={img} alt="divine" />}
             </div>
 
-            <p className="pDivine">
-              The Vindicators are souls torn from The Arc. During the
-              Eradication, the Arc realized they were outnumbered and forced
-              their soul into separate beings to independently grow stronger.
-              The downside, they are mortal, and many were lost in the battle.
-              The remaining Vindicators stand ready for the next Eradication and
-              ready to give their life protecting the Heavens.
-            </p>
+            <p className="pDivine">{text}</p>
           </div>
         </div>
       )}
